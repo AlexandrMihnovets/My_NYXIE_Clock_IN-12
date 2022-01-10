59bfd18d609ac4145d1edccdee75960d2da7866f
Alexandr <sasha_mihnovets@mail.ru>
2022-01-10 15:39:52 +0500

    Start!

---
+++ main/main/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ * Created: 10.01.2022 15:29:55
+ * Author: MikhnovetsA
+ * Программа для часов на газоразрядных индикаторах ИН-12 и МК ATmega8(88) на языке Си
+ *
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
